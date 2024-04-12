5699fbcf41d6111d08038a2f095e2a84b4bfa8d0
Matthew Wierenga <matt.wierenga@outlook.com>
2024-04-12 11:52:25 +0200

    Added NPC script
    to see which NPC you are talking to

---
+++ Unity/Cape Flat Chronicles/Assets/Scripts/NPCS/NPCDialogueManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogueManager : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class NPC
+    {
+        public string npcName;
+        public string[] dialogueLines;
+    }
+
+    public List<NPC> npcList = new List<NPC>();
+
+
+    public void DiaplayDialogue(string npcName)
+    {
+
+        // Find the NPC in the list.
+        NPC npc = npcList.Find(n => n.npcName == npcName);
+        if (npc != null)
+        {
+            // Display the NPC's dialogue.
+            foreach (string line in npc.dialogueLines)
+            {
+                Debug.Log(line);
+                // Here you would add your code to display the dialogue line to the player.
+            }
+        }
+        else
+        {
+            Debug.LogError("NPC not found!");
+        }
+    }
+
+}
+
+
+
--- Unity/Cape Flat Chronicles/Assets/Scripts/NPCS/NPCInteractable.cs
+++ Unity/Cape Flat Chronicles/Assets/Scripts/NPCS/NPCInteractable.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour
 {
+    public string npcName; // The name of the NPC
+
+    public NPCDialogueManager dialogueManager;
 
     public void Interact()
     {
-        Debug.Log("Interact");
+        
+
+        // Call the DisplayDialogue method with this NPC's name
+        if (dialogueManager != null)
+        {
+            dialogueManager.DiaplayDialogue(npcName);
+            Debug.Log(npcName);
+        }
+        else
+        {
+            Debug.LogError("NPCDialogueManager not found in the scene.");
+        }
     }
 }
